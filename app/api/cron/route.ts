3a58f45bd485fdd7afff37e6b1b82efcd19abbc0
frxsav <keccofor@gmail.com>
2026-01-30 12:30:56 +0100

    Vercel check

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -8,6 +8,7 @@ export async function GET(request: NextRequest) {
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     return new Response('Non autorizzato', { status: 401 });
   }
+  // afadfa f
 
   // 2. Chiamata al database per risvegliarlo così che Supabase non lo disattiva per inattività
   // La chiamata chiede quante righe ci sono nella tabella "user"
