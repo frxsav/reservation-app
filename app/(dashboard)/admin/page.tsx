2f51b399a2c272ac5f36c43c2f28098f6391fc7e
frxsav <keccofor@gmail.com>
2026-05-09 22:52:34 +0200

    fixed form show

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -7,17 +7,23 @@ export default async function Admin() {
   return (
     <div className="h-screen flex flex-col items-center mt-24 gap-12">
       <div className="flex flex-col items-center">
-        <span className="text-4xl font-bold text-green">{session?.user?.role}</span>
-        <h1 className="text-3xl text-background font-bold text-shadow-md text-shadow-dark">
+        <span className="text-4xl font-bold text-green">
+          {session?.user?.role}
+        </span>
+        <h1 className="text-4xl text-background font-bold [text-shadow:0_2px_4px_rgba(0,0,0,0.5),0_4px_16px_rgba(0,0,0,0.3)]">
           {session?.user
             ? `Benvenuto ${session?.user?.name}`
-            : `Esegui il login per vedere questa pagina`}
+            : `Effettua il login per vedere questa pagina`}
         </h1>
       </div>
-      <div className="p-4 border-2 border-green flex flex-col items-center gap-8 bg-stone/80 backdrop-blur-xs">
-        <span className='text-background text-xl'>Compila il form per creare una nuova prenotazione</span>
-        <BookingForm />
-      </div>
+      {session?.user && (
+        <div className="p-4 border-2 border-green flex flex-col items-center gap-8 bg-stone/80 backdrop-blur-xs">
+          <span className="text-background text-xl">
+            Compila il form per creare una nuova prenotazione
+          </span>
+          <BookingForm />
+        </div>
+      )}
     </div>
   );
 }
